da3a3511a7f05687bc73dba4f0363a3f29e9a3e1
jacopomanenti01 <jacopomanenti01@gmail.com>
2025-04-22 22:53:07 +0200

    Quartz sync: Apr 22, 2025, 10:53 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -4,7 +4,12 @@ import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+  return <div id="quartz-body">
+
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WFJ3RFVH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+    {children}</div>
 }
 
 Body.afterDOMLoaded = clipboardScript
