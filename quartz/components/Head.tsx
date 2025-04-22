e47c804d72431fd0d633b0ad7e76c688008c89bf
jacopomanenti01 <jacopomanenti01@gmail.com>
2025-04-22 22:55:00 +0200

    Quartz sync: Apr 22, 2025, 10:55 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -156,13 +156,18 @@ export default (() => {
 
     return (
       <head>
-        <script>
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WFJ3RFVH');
-        </script>
+        <script dangerouslySetInnerHTML={{
+        __html: `
+        (function(w,d,s,l,i){
+          w[l]=w[l]||[];
+          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WFJ3RFVH');
+        `
+      }} />
         <title>{title}</title>
         <meta charSet="utf-8" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
